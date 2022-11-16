54637af0b7f01f67f84041c31c087fbea60e9d2c
Md Ashraful Islam <ashraful.97mdp@gmail.com>
2022-11-16 13:04:51 +0600

    edit

--- models/item_receive.py
+++ models/item_receive.py
@@ -2,14 +2,15 @@ from odoo import api, fields, models, _
 from datetime import date
 from odoo.exceptions import UserError
 
+
 class StockPickingOperation(models.Model):
     _inherit = "stock.picking"
     _order = 'name desc'
     serial = fields.Char(string='Order No', required=True, copy=False, readonly=True,
-                           default=lambda self: _('New'))
+                         default=lambda self: _('New'))
     service_order_id = fields.Many2one('field.service', string="Service Order")
     transfer_to_branch = fields.Many2one('res.branch', string="To Branch")
-    contact_person=fields.Char('res.partner')
+    contact_person = fields.Many2one('res.partner')
     state = fields.Selection([
         ('draft', 'Draft'),
         ('approval', 'Approval'),
@@ -20,34 +21,31 @@ class StockPickingOperation(models.Model):
         ('cancel', 'Cancelled'), ])
     custom_operation_receive = fields.Boolean(default=False)
     custom_operation_transfer = fields.Boolean(default=False)
-
     picking_type_id = fields.Many2one(
         'stock.picking.type', 'Operation Type',
         required=True, readonly=True,
         default=lambda self: self._set_operation_type(),
         states={'draft': [('readonly', False)]})
-    received=fields.Boolean(related='service_order_id.receive_customer',string="received status")
-
+    received = fields.Boolean(related='service_order_id.receive_customer', string="received status")
     branch_id = fields.Many2one('res.branch', default=lambda self: self.env.user.branch_id)
 
-
     def _set_operation_type(self):
-
-        if 'default_custom_operation_transfer' in self.env.context.keys() and self.env.context.get('default_custom_operation_transfer') == True:
+        if 'default_custom_operation_transfer' in self.env.context.keys() and self.env.context.get(
+                'default_custom_operation_transfer') == True:
             return self.env['stock.picking.type'].search(
                 [('warehouse_id', '=', self.env.user.context_default_warehouse_id.id),
-                 ('code', '=', 'internal')],limit=1).id
-        elif 'default_custom_operation_receive' in self.env.context.keys() and self.env.context.get('default_custom_operation_receive') == True:
+                 ('code', '=', 'internal')], limit=1).id
+        elif 'default_custom_operation_receive' in self.env.context.keys() and self.env.context.get(
+                'default_custom_operation_receive') == True:
 
             return self.env['stock.picking.type'].search(
                 [('warehouse_id', '=', self.env.user.context_default_warehouse_id.id),
-                 ('code', '=', 'incoming')],limit=1).id
+                 ('code', '=', 'incoming')], limit=1).id
         else:
             None
 
-
     @api.onchange('service_order_id')
-    def onchange_order(self):
+    def onchange_service_order(self):
         user = self.env['res.users'].browse(self._context.get('uid'))
         val_list = []
         warehouse_data = self.env['stock.warehouse'].search([
@@ -65,15 +63,14 @@ class StockPickingOperation(models.Model):
                 'location_dest_id': self.location_dest_id.id,
                 'branch_id': user.branch_id.id,
                 'order_ref': self.service_order_id.customer_id.id,
-                'order_id' : self.service_order_id.id,
-                'serial_no' : self.service_order_id.imei_no
+                'order_id': self.service_order_id.id,
+                'serial_no': self.service_order_id.imei_no
 
             })
             print(self.service_order_id.customer_id.id)
             val_list.append(vals)
 
         self.move_ids_without_package = val_list
-
 
     @api.onchange("transfer_to_branch")
     def onchange_transfering_branch(self):
@@ -87,25 +84,22 @@ class StockPickingOperation(models.Model):
             ('company_id', '=', user.company_id.id),
 
         ])
-        contact_person=self.env['res.users'].search([('branch_id','=',self.transfer_to_branch.id)])
-        print(contact_person.name)
-
+        # contact_person=self.env['res.partner'].search([('branch_id','=',self.transfer_to_branch)])
+        # print(contact_person.name)
         s = self.env['stock.picking.type'].search(
-                [('warehouse_id', '=', warehouse_data.id),
-                 ('code', '=', 'incoming')], limit=1).default_location_dest_id
+            [('warehouse_id', '=', warehouse_data.id),
+             ('code', '=', 'incoming')], limit=1).default_location_dest_id
         if s:
-
             self.location_dest_id = s
-            #self.partner_id=contact_person.id.name
+            # self.partner_id=contact_person.name
 
     @api.model
     def create(self, vals):
-
         res = super(StockPickingOperation, self).create(vals)
         if vals.get('serial', _('New')) == _('New'):
             if res.picking_type_id.code == 'incoming':
-                val=self.env['ir.sequence'].next_by_code('so.receive') or _('New')
-                res.name=val
+                val = self.env['ir.sequence'].next_by_code('so.receive') or _('New')
+                res.name = val
 
         if res.picking_type_id.code == 'internal':
             val = self.env['ir.sequence'].next_by_code('so.transfer') or _('New')
@@ -114,13 +108,16 @@ class StockPickingOperation(models.Model):
         so = self.env['field.service'].sudo().search([('id', '=', so_id)])
         if res.picking_type_id.code == 'incoming':
             so.receive_customer = True
-            so.product_receive_date = self.scheduled_date
-            so.x= True
+            so.product_receive_date = res.scheduled_date
+            so.x = True
             so.item_receive_status = "Received"
-            so.product_receive_date = self.scheduled_date
-            print(so.product_receive_date,self.scheduled_date)
+            so.product_receive_date = res.scheduled_date
+            print(so.product_receive_date, self.scheduled_date)
         elif res.picking_type_id.code == 'internal':
             so.so_transfer = True
+            print(res.transfer_to_branch)
+            so.current_branch = res.transfer_to_branch.id
+            print(so.current_branch)
             so.item_receive_status = "Transfered"
         return res
 
@@ -136,23 +133,20 @@ class StockPickingOperation(models.Model):
 
         return {
 
-
             'name': ('Item Transfer'),
             'view_mode': 'tree,form',
             # 'view_type': 'tree,form',
             'views': [(tree_id, 'tree'), (form_id, 'form')],
             # 'context': {'default_picking_type_id': p.id},
             'res_model': 'stock.picking',
-            'context': {'default_picking_type_id': p.id,'default_branch_id': user.branch_id.id,
+            'context': {'default_picking_type_id': p.id, 'default_branch_id': user.branch_id.id,
                         'default_service_order_id': self.service_order_id.id},
             'type': 'ir.actions.act_window',
             'domain': [('picking_type_id', '=', p.id)],
             'target': 'current',
             'res_id': self.id,
 
-
         }
-
 
     def action_transfer_order_receive_operation(self):
         form_id = self.env.ref('usl_service_erp.view_picking_form_field_service_transfer_order_receive').id,
@@ -173,19 +167,26 @@ class StockPickingOperation(models.Model):
             'views': [(tree_id, 'tree'), (form_id, 'form')],
 
             'res_model': 'stock.picking',
-            #'context': {'default_picking_type_id': p.id,'default_branch': user.branch_id.id,
-                        #'default_service_order_id': self.service_order_id.id},
-            'domain': [('transfer_to_branch', '=',user.branch_id.id)],
+            # 'context': {'default_picking_type_id': p.id,'default_branch': user.branch_id.id,
+            # 'default_service_order_id': self.service_order_id.id},
+            'domain': [('transfer_to_branch', '=', user.branch_id.id)],
 
             # 'res_id': self.id,
 
         }
 
-
+    # @api.onchange('transfer_to_branch')
+    # def onchange_transfer_to_branch(self):
+    #     for rec in self:
+    #
+    #         print("------------------------------>",rec)
+    #         return {'domain': {'contact_person': [('transfer_to_branch', '=', rec.transfer_to_branch)]}}
 
     def action_operation_receive(self):
         print("hello")
         return
+
+
 
     # @api.onchange('transfer_to_branch')
     # def onchange_transfer_to_branch(self):
@@ -195,17 +196,15 @@ class StockPickingOperation(models.Model):
     #         return {'domain': {'partner_id': [('branch_id', '=', self.transfer_to_branch.id)]}}
 
 
-
 class stockmove(models.Model):
     _inherit = 'stock.move'
     order_ref = fields.Many2one('res.partner', string="Receive from")
-    order_id =  fields.Many2one('field.service', string="Order Id")
+    order_id = fields.Many2one('field.service', string="Order Id")
     serial_no = fields.Char(string="Serial No")
 
 
-#from odoo import api, fields, models, _
+# from odoo import api, fields, models, _
 from datetime import date
-
 
 # class StockPickingOperation(models.Model):
 #     _inherit = "stock.picking"
